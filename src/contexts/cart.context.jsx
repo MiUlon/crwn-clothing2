f7dec3b4eceaa6cfed9e629ac124af878779246a
MiUlon <mindegi@gmail.com>
2023-10-30 12:02:36 +0200

    implemented function to decrease cart items quantity

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -13,12 +13,26 @@ export const addCartItem = (cartItems, productToAdd) => {
     return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+export const removeCartItem = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id);
+
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+    };
+
+    return cartItems.map((cartItem) => cartItem.id === cartItemToRemove.id ?
+        { ...cartItem, quantity: cartItem.quantity - 1 } :
+        cartItem
+    );
+};
+
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {},
+    setIsCartOpen: () => { },
     cartItems: [],
-    addItemToCart: () => {},
-    cartCount: 0 
+    addItemToCart: () => { },
+    removeItemFromCart: () => { },
+    cartCount: 0
 });
 
 export const CartProvider = ({ children }) => {
@@ -30,12 +44,16 @@ export const CartProvider = ({ children }) => {
         const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
         setCartCount(newCartCount);
     }, [cartItems]);
-    
+
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
     };
-    
-    const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount };
-    
-    return <CartContext.Provider value={value}>{children}</CartContext.Provider>
+
+    const removeItemFromCart = (cartItemToRemove) => {
+        setCartItems(removeCartItem(cartItems, cartItemToRemove));
+    };
+
+const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount, removeItemFromCart };
+
+return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 };
